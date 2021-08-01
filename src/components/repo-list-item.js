219d14bcba6cd015d6f725376c410d253757411b
Viraj Patidar <vrj381@gmail.com>
2021-08-01 17:42:28 +0530

    repo list + items styled

--- src/components/repo-list-item.js
+++ src/components/repo-list-item.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from './repo-list-item.module.scss';
 
 const RepoListItem = ({ repo }) => {
-  return (
-    <div>
-      <div>
-        <Link href="/repo/[id]" as={`/repo/${repo.id}`}>
-          <a>{repo.name}</a>
-        </Link>
-        <p>{repo.description}</p>
-      </div>
-    </div>
-  );
+    const cutDescription = (description) => {
+        if (description !== null && description.length > 100) {
+            return description.slice(0, 100) + '...';
+        }
+
+        return description;
+    };
+
+    return (
+        <div className={`${styles.repoListItem} box`}>
+            <div className={styles.repoName}>
+                <Link href="/repo/[id]" as={`/repo/${repo.id}`}>
+                    <a>{repo.name}</a>
+                </Link>
+            </div>
+
+            <p className={styles.className}>{cutDescription(repo.description)}</p>
+
+            <div className={styles.footer}>
+                <div className={styles.counters}>
+                    <div className={styles.counter}>
+                        <span>{repo.stargazers_count}</span>
+                    </div>
+                    <div className={styles.counter}>
+                        <span>{repo.forks_count}</span>
+                    </div>
+                </div>
+                <div className={styles.language}>
+                    <spa>{repo.language}</spa>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default RepoListItem;
