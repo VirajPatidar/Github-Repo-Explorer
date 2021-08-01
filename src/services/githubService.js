648adff88e8c5c28b3f2f61e9a9722c43dc73494
Viraj Patidar <vrj381@gmail.com>
2021-08-01 13:51:25 +0530

    implement search from api folder

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { axiosGetCancellable } from './axios.helper';
 
 const axiosConfig = {
     baseURL: 'https://api.github.com/'
@@ -11,10 +12,18 @@ const axiosConfig = {
 function searchRepos(searchText, language) {
     const query = language ? `${searchText}+language:${language}` : searchText;
 
-    return axios.get(
-        `search/repositories?q=${query}&sort=stars&order=desc`,
-        axiosConfig
-    );
+    if (isServer()) {
+        return axios.get(
+            `search/repositories?q=${query}&sort=stars&order=desc`,
+            axiosConfig
+        );
+    }
+
+    return axiosGetCancellable(`api/search?q=${query}&sort=stars&order=desc`);
+}
+
+function isServer() {
+    return typeof window === 'undefined';
 }
 
 export { searchRepos };
