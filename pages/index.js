ae9e7db550ed86ae4563d6e278b018decd5b795c
Viraj Patidar <vrj381@gmail.com>
2021-08-01 12:36:51 +0530

    minor fixes

--- pages/index.js
+++ pages/index.js
@@ -5,39 +5,41 @@ import Search from '../src/components/search';
 import { searchRepos } from '../src/services/githubService';
 
 const Index = (props) => {
-  const [searchText, setSearchText] = useState('');
-  const [language, setLanguage] = useState('');
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const [searchText, setSearchText] = useState('');
+    const [language, setLanguage] = useState('');
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  const onSearchTextChange = (text) => {
-    setSearchText(text);
-    loadRepos(text, language);
-  };
+    const onSearchTextChange = (text) => {
+        setSearchText(text);
+        if (text) {
+            loadRepos(text, language);
+        }
+    };
 
-  const onLanguageChange = (language) => {
-    setLanguage(language);
-    loadRepos(searchText, language);
-  };
+    const onLanguageChange = (language) => {
+        setLanguage(language);
+        loadRepos(searchText, language);
+    };
 
-  const loadRepos = async (searchText, language) => {
-    setLoading(true);
-    const res = await searchRepos(searchText, language);
-    setLoading(false);
-    setRepos(res.data.items);
-  };
+    const loadRepos = async (searchText, language) => {
+        setLoading(true);
+        const res = await searchRepos(searchText, language);
+        setLoading(false);
+        setRepos(res.data.items);
+    };
 
-  return (
-    <div>
-      <Search
-        searchText={searchText}
-        language={language}
-        onSearchTextChange={onSearchTextChange}
-        onLanguageChange={onLanguageChange}
-      />
-      {loading ? 'Loading...' : <div>{JSON.stringify(repos, null, 2)}</div>}
-    </div>
-  );
+    return (
+        <div>
+            <Search
+                searchText={searchText}
+                language={language}
+                onSearchTextChange={onSearchTextChange}
+                onLanguageChange={onLanguageChange}
+            />
+            {loading ? 'Loading...' : <div>{JSON.stringify(repos, null, 2)}</div>}
+        </div>
+    );
 };
 
 export default Index;
